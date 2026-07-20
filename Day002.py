652a2802be5a02c28ce7263ebed38a4623157a30
Keshav Bhardwaj <keshavbhardwaj2800@gmail.com>
2026-07-20 15:14:11 +0530

    DAY 2

--- Day002.py
+++ Day002.py
@@ -1,1 +1,53 @@
- 
+#         --Operations on Strings--
+
+# a = "Keshav"
+# print(a[3])
+
+# print(a[0:5])
+
+# print(a[-5:-1])
+
+#         --String methods--
+
+# print(len(a)) 
+
+# print(a.upper())
+
+# print(a.lower())
+
+# b = "Keshav#,######,Harry"
+# c = b.rstrip("#")
+
+# print(c)
+
+# d = b.replace("Kesh", "Resh")
+
+# print(d)
+
+# e = b.split(",")
+# print(e)
+
+f = "introducTion to JS"
+
+g = f.capitalize()
+
+print(g)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
