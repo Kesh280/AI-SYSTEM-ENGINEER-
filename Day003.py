a9c9eca3a8170034b93dbf609fcac3d39300cea8
Keshav Bhardwaj <keshavbhardwaj2800@gmail.com>
2026-07-21 13:53:09 +0530

    DAY 3

---
+++ Day003.py
@@ -0,0 +1,33 @@
+#          --LOOPS--
+
+
+#          --For Loops--
+
+Name = "Keshav"
+
+for i in Name:
+    print(i)
+
+colors = ["Red","Green","Blue","Violet"]
+
+for x in colors:
+    print(x)
+    for i in x:
+        print(i)
+
+#            --Range--
+
+for i in range(10):
+   print(i)
+
+for i in range(10):
+   print(i+1)
+
+for i in range(1,11):
+    print(i)
+
+for i in range(1,15,2):
+    print(i)
+
+
+
