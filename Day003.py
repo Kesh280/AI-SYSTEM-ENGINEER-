69857c46844d4926aa0da7a5a946310393fc965f
Keshav Bhardwaj <keshavbhardwaj2800@gmail.com>
2026-07-21 22:41:08 +0530

    DAY 3 Complete

--- Day003.py
+++ Day003.py
@@ -57,5 +57,63 @@ while(i<3):
 
 print("moved out of the loop")
 
+i = int(input("Enter the number: "))
+while(i>0):
+    print(i)
+    i = i - 1
+
+
+i = int(input("Enter the number: "))
+while(i<=32):
+    i = int(input("Enter the number: "))
+    print(i)
+    
+print("Done ")    
+
+i = 0
+while(i<9):
+    print(i)
+    i = i + 1
+
+else:
+    print("The loop is done")
+
+# --------------------------------------------
+
+a = range(10)
+
+for i in a:
+    if (i == 6):
+        print("we reached")
+        break
+    print(i)
+
+for i in a:
+    if (i == 6):
+        print("Special number")
+        continue
+    print(i)
+
+for i in range(11):
+     print("5","x",i,"=",5*i) #Table of 5 by for loop
+
+for i in range(11):
+     if (i == 5):
+          break
+     else:
+      print("5","x",i,"=",5*i)
+
+i = 0
+
+while(i<11):
+    print("5","x",i,"=",5*i) #Table of 5 by while loop
+    
+    i = i + 1
+
+     
+
+
+
+
 
 
